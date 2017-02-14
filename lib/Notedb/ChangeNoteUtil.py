d2e28c40ad2a96f55b12239061b3bf61f10121b6
Zara Zaimeche <zara.zaimeche@codethink.co.uk>
2017-02-14 13:47:51 +0000

    Update ChangeNoteUtil to make a bit more sense

--- lib/Notedb/ChangeNoteUtil.py
+++ lib/Notedb/ChangeNoteUtil.py
@@ -12,16 +12,30 @@ FOOTER_SUBJECT= 'Subject'
 FOOTER_TOPIC = 'Topic'
 FOOTER_LABEL = 'Label'
 FOOTER_REMOVED = 'Removed'
+# original says this is not a complete set of footers. so repeating that here
+# so it doesn't get forgotten.
 
 class ChangeNoteUtil(object):
     def __init__(self):
         pass
 
-    def change_ref_name():
-        class, change_id, ref_prefix = [] #not sure about original syntax here
+    def change_ref_name(class, change_id, ref_prefix):
         refname = []
+    
         # The code here is different from gerrit's notedb;
         # not sure if we want it exactly the same or not
+        # for now I've left this.
+        prefix = ref_prefix
     
-    def change_id():
-        pass 
+    def change_id(class, change_meta_ref):
+         if change_meta_ref = '': # SHOULD BE REGEXP HALP
+             change_id = ChangeId()
+             bit_of_thing_matched_in_brackets_in_regexp = 'todo'
+             # guess 'looks_like_sha' is perl's 'looks_like_number',
+             # that has been extended somehow
+             if:
+                 looks_like_sha(bit_of_thing_matched_in_brackets_in_regexp)
+                 is_uuid = True
+                 change_id.id = bit_of_thing_matched_in_brackets_in_regexp
+                 return change_id
+            return None #this is return undef in original
